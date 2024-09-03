398ee53c175f26a6b9530a5dd83ffe75a2a22e6d
karol <karolw881@gmail.com>
2024-09-03 10:00:58 +0200

    nowe kontrolery, poprawione bazy

--- demo1/src/main/java/com/example/demo/Demo1Application.java
+++ demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -13,12 +13,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "com.example.demo")
 
 public class Demo1Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
-
-
-
 	}
 
 }
--- demo1/src/main/java/com/example/demo/controles/Maincontroller.java
+++ demo1/src/main/java/com/example/demo/controles/Maincontroller.java
@@ -6,41 +6,59 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+
 @RequestMapping("/api")
 @RestController
 public class Maincontroller {
 
-    private final ServiceZamowienie serviceZamowienie;
+    private final ServiceZamowienie serviceZamowienie; // definiuj Serwis w kontrolerze
 
     public Maincontroller(ServiceZamowienie serviceZamowienie) {
         this.serviceZamowienie = serviceZamowienie;
     }
 
-
-
+    @CrossOrigin
     @GetMapping("/List")
-    public String zamowieniaList() {
-        List<Zamowienie> zamowienies =  serviceZamowienie.getAllZamowienie();
-        return zamowienies.toString();
+    public ResponseEntity<List<Zamowienie>> zamowieniaList() {
+        List<Zamowienie> zamowienies = serviceZamowienie.getAllZamowienie();
+        return ResponseEntity.ok(zamowienies);
     }
 
 
+    @GetMapping("/List/{id}")
+    public ResponseEntity<Zamowienie> zamowienieListId(@PathVariable Long id) {
+        Optional<Zamowienie> znalazlemID = serviceZamowienie.getIdZamowienie(id);
+        return znalazlemID.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @CrossOrigin
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteZamowienie(@PathVariable Long id) {
         serviceZamowienie.delete(id);
         return ResponseEntity.ok("Zamowienie o ID " + id + " zostało usunięte.");
     }
 
+    @CrossOrigin
     @PostMapping("/save")
-    public ResponseEntity<Zamowienie> zamowieniaSave() {
-        Zamowienie zamowienie = new Zamowienie(100,"a","b",1);
+    public ResponseEntity<Zamowienie> zamowieniaSave(@RequestBody Zamowienie zamowienie) {
         Zamowienie savedZamowienie = serviceZamowienie.createZamowienie(zamowienie);
         return ResponseEntity.ok(savedZamowienie);
     }
 
 
+    @CrossOrigin
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Zamowienie> updateZamowienie(@PathVariable Long id, @RequestBody Zamowienie zamowienie) {
+        Zamowienie updatedZamowienie = serviceZamowienie.updateZamowienie(id, zamowienie);
+        return ResponseEntity.ok(updatedZamowienie);
+    }
 
-
-
-
+    @CrossOrigin
+    @GetMapping("/LastOrder")
+    public ResponseEntity<Zamowienie> getLastOrder() {
+     Zamowienie zamowienie = serviceZamowienie.getLastZamowienie();
+        return ResponseEntity.ok(zamowienie);
+    }
 }
